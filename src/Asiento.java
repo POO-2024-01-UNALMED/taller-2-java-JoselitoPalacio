0d076e0ba024e0b5fc9c0abbd1565043ba477013
JoselitoPalacio <jpalaciose@unal.edu.co>
2024-02-20 02:09:36 -0500

    k

--- src/Asiento.java
+++ src/Asiento.java
@@ -1,7 +1,15 @@
-package test;
 public class Asiento {
     int precio;
     String color;
     int registro;
-    
+    void cambiarColor(String color) {
+        String[] colors = {"rojo", "verde", "amarillo", "negro", "blanco"};
+        for (String colores : colors) {
+            if (color.equalsIgnoreCase(colores)) {
+                this.color = color;
+                return;
+            }
+        }
+        System.out.println("Color no permitido.");
+    }
 }
